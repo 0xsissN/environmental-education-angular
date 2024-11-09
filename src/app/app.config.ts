2f8915727878bd600fe16e2c2df6bc8663e9cbb9
OracleShadow <nico22delcastillo07@gmail.com>
2024-11-09 15:54:07 -0400

    configuration env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(), provideFirebaseApp(() =>
       initializeApp({
-        apiKey: "AIzaSyAWyobMe9sjDsNL3Ij-kZ6Sng76pKjzkMY",
-        authDomain: "environmental-education-30f7b.firebaseapp.com",
-        projectId: "environmental-education-30f7b",
-        storageBucket: "environmental-education-30f7b.appspot.com",
-        messagingSenderId: "706397050741",
-        appId: "1:706397050741:web:295013dc19eed7e7b5a53c"      
+        apiKey: environment.apiKey,
+        authDomain: environment.authDomain,
+        projectId: environment.projectId,
+        storageBucket: environment.storageBucket,
+        messagingSenderId: environment.messagingSenderId,
+        appId: environment.appId      
       })
     ), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
